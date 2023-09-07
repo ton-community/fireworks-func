3ea8bf172d45f16804772b2cc7ec6b6a2bc2e256
AlexG <39581753+Reveloper@users.noreply.github.com>
2023-09-07 12:04:33 +0300

    test_update

--- tests/Fireworks.spec.ts
+++ tests/Fireworks.spec.ts
@@ -1,5 +1,5 @@
 import { Blockchain, SandboxContract } from '@ton-community/sandbox';
-import { Cell, toNano } from 'ton-core';
+import { Cell, toNano, beginCell } from 'ton-core';
 import { Fireworks } from '../wrappers/Fireworks';
 import '@ton-community/test-utils';
 import { compile } from '@ton-community/blueprint';
@@ -82,7 +82,29 @@ describe('Fireworks', () => {
             op: 0x6efe144b //launch_first
         })
 
-        console.log(launchResult.transactions[2]);
+        const init_code = code;
+        const init_data = beginCell().storeUint(1, 32).endCell();
+
+        const state_init = beginCell()
+            .storeUint(0, 1) //no split_depth
+            .storeUint(0, 1) // no special
+            .storeUint(1, 1) // we have code
+            .storeRef(init_code)
+            .storeUint(1, 1) // we have data
+            .storeRef(init_data)
+            .storeUint(0, 1) // we have no library
+            .endCell();
+
+        const hash_dst = state_init.hash
+        if (hash_dst === null) {
+            throw Error("wrong type");
+        }
+
+        let address = beginCell().storeInt(0, 8).storeUint(hash_dst. , 256).endCell();
+
+        slice dest_address = begin_cell().store_int(0, 8).store_uint(state_init_hash, 256).end_cell().begin_parse();
+
+
 
     });
 });
