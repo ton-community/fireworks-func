9f59a4d6bc5566ae077c91e1a8468ed4d79190f1
Spite <spite.moriarty@gmail.com>
2024-01-11 06:41:37 +0400

    refactoring + add 1 & 2 exit codes

--- tests/Fireworks.EdgeCases.spec.ts
+++ tests/Fireworks.EdgeCases.spec.ts
@@ -6,7 +6,7 @@ import {
     TreasuryContract,
 } from '@ton-community/sandbox';
 import { Cell, toNano, beginCell, Address } from 'ton-core';
-import { Fireworks, OPCODES } from '../wrappers/Fireworks';
+import { ExitCode, Fireworks, OPCODES } from '../wrappers/Fireworks';
 import '@ton-community/test-utils';
 import { compile } from '@ton-community/blueprint';
 
@@ -24,13 +24,13 @@ describe('Edge Cases Tests', () => {
         code = await compile('Fireworks');
 
         blockchain = await Blockchain.create();
-        blockchain.verbosity = {
-            ...blockchain.verbosity,
-            blockchainLogs: true,
-            vmLogs: 'vm_logs_full',
-            debugLogs: true,
-            print: false,
-        };
+        // blockchain.verbosity = {
+        //     ...blockchain.verbosity,
+        //     blockchainLogs: true,
+        //     vmLogs: 'vm_logs_full',
+        //     debugLogs: true,
+        //     print: false,
+        // };
 
         launcher = await blockchain.treasury('launcher');
 
@@ -89,16 +89,33 @@ describe('Edge Cases Tests', () => {
         await blockchain.loadFrom(initialState);
     });
 
-    it('transaction should aborted with exit code = 9 ', async () => {
-        const launchResult = await fireworks.sendBadMessage1(launcher.getSender(), toNano('2.0'));
+    it('transaction should aborted with exit code = 1 ', async () => {
+        const body = beginCell().storeUint(OPCODES.FAKED_LAUNCH, 32).storeUint(ExitCode.SuccessAlt, 8).endCell();
+        const launchResult = await fireworks.sendBadMessage(launcher.getSender(), toNano('2.0'), body);
+
+        expect(launchResult.transactions).toHaveTransaction({
+            from: launcher.address,
+            to: fireworks.address,
+            success: true,
+            exitCode: ExitCode.SuccessAlt,
+            // exit code = 8  Cell overflow.  Writing to builder is not possible since after operation there would be more than 1023 bits or 4 references.. https://docs.ton.org/learn/tvm-instructions/tvm-exit-codes
+            op: OPCODES.FAKED_LAUNCH,
+        });
+
+        await blockchain.loadFrom(initialState);
+    });
+
+    it('transaction should aborted with exit code = 2 ', async () => {
+        const body = beginCell().storeUint(OPCODES.FAKED_LAUNCH, 32).storeUint(ExitCode.StackUnderflow, 8).endCell();
+        const launchResult = await fireworks.sendBadMessage(launcher.getSender(), toNano('2.0'), body);
 
         expect(launchResult.transactions).toHaveTransaction({
             from: launcher.address,
             to: fireworks.address,
             success: false,
             aborted: true,
-            exitCode: 9,
-            // exit code = 9 Stack underflow. Last TVM op-code consumed more elements than there are on the stacks. https://docs.ton.org/learn/tvm-instructions/tvm-exit-codes
+            exitCode: ExitCode.StackUnderflow,
+            // exit code = 8  Cell overflow.  Writing to builder is not possible since after operation there would be more than 1023 bits or 4 references.. https://docs.ton.org/learn/tvm-instructions/tvm-exit-codes
             op: OPCODES.FAKED_LAUNCH,
         });
 
@@ -106,15 +123,41 @@ describe('Edge Cases Tests', () => {
     });
 
     it('transaction should aborted with exit code = 8 ', async () => {
-        const launchResult = await fireworks.sendBadMessage2(launcher.getSender(), toNano('2.0'));
+        const body = beginCell()
+            .storeUint(OPCODES.FAKED_LAUNCH, 32)
+            .storeUint(ExitCode.CellOverflow, 8)
+            .storeRef(beginCell().storeUint(1, 256).storeUint(2, 256).storeUint(3, 256).storeUint(4, 255).endCell()) // 1023 bits cell
+            .endCell();
+        const launchResult = await fireworks.sendBadMessage(launcher.getSender(), toNano('2.0'), body);
 
         expect(launchResult.transactions).toHaveTransaction({
             from: launcher.address,
             to: fireworks.address,
             success: false,
             aborted: true,
-            exitCode: 8,
+            exitCode: ExitCode.CellOverflow,
             // exit code = 8  Cell overflow.  Writing to builder is not possible since after operation there would be more than 1023 bits or 4 references.. https://docs.ton.org/learn/tvm-instructions/tvm-exit-codes
+            op: OPCODES.FAKED_LAUNCH,
+        });
+
+        await blockchain.loadFrom(initialState);
+    });
+
+    it('transaction should aborted with exit code = 9 ', async () => {
+        const body = beginCell()
+            .storeUint(OPCODES.FAKED_LAUNCH, 32)
+            .storeUint(ExitCode.CellUnderflow, 8)
+            .storeRef(beginCell().endCell())
+            .endCell();
+        const launchResult = await fireworks.sendBadMessage(launcher.getSender(), toNano('2.0'), body);
+
+        expect(launchResult.transactions).toHaveTransaction({
+            from: launcher.address,
+            to: fireworks.address,
+            success: false,
+            aborted: true,
+            exitCode: ExitCode.CellUnderflow,
+            // exit code = 9 Stack underflow. Last TVM op-code consumed more elements than there are on the stacks. https://docs.ton.org/learn/tvm-instructions/tvm-exit-codes
             op: OPCODES.FAKED_LAUNCH,
         });
 
--- wrappers/Fireworks.ts
+++ wrappers/Fireworks.ts
@@ -15,6 +15,53 @@ export const OPCODES = {
     FAKED_LAUNCH: 0x39041457,
 };
 
+/*
+0	Compute Phase	Standard successful execution exit code.
+1	Compute Phase	Alternative successful execution exit code.
+2	Compute Phase	Stack underflow. Last op-code consumed more elements than there are on the stacks. 1
+3	Compute Phase	Stack overflow. More values have been stored on a stack than allowed by this version of TVM.
+4	Compute Phase	Integer overflow. Integer does not fit into −2256 ≤ x < 2256 or a division by zero has occurred.
+5	Compute Phase	Integer out of expected range.
+6	Compute Phase	Invalid opcode. Instruction is unknown in the current TVM version.
+7	Compute Phase	Type check error. An argument to a primitive is of an incorrect value type. 1
+8	Compute Phase	Cell overflow. Writing to builder is not possible since after operation there would be more than 1023 bits or 4 references.
+9	Compute Phase	Cell underflow. Read from slice primitive tried to read more bits or references than there are.
+10	Compute Phase	Dictionary error. Error during manipulation with dictionary (hashmaps).
+11	Compute Phase	Most oftenly caused by trying to call get-method whose id wasn't found in the code (missing method_id modifier or wrong get-method name specified when trying to call it). In TVM docs its described as "Unknown error, may be thrown by user programs".
+12	Compute Phase	Thrown by TVM in situations deemed impossible.
+13	Compute Phase	Out of gas error. Thrown by TVM when the remaining gas becomes negative.
+-14	Compute Phase	It means out of gas error, same as 13. Negative, because it cannot be faked
+32	Action Phase	Action list is invalid. Set during action phase if c5 register after execution contains unparsable object.
+-32	Action Phase	(the same as prev 32) - Method ID not found. Returned by TonLib during an attempt to execute non-existent get method.
+33	Action Phase	Action list is too long.
+34	Action Phase	Action is invalid or not supported. Set during action phase if current action cannot be applied.
+37	Action Phase	Not enough TON. Message sends too much TON (or there is not enough TON after deducting fees).
+38	Action Phase	Not enough extra-currencies.
+ */
+
+export enum ExitCode {
+    Success = 0,
+    SuccessAlt = 1,
+    StackUnderflow = 2,
+    StackOverflow = 3,
+    IntegerOverflow = 4,
+    IntegerOutOfRange = 5,
+    InvalidOpcode = 6,
+    TypeCheckError = 7,
+    CellOverflow = 8,
+    CellUnderflow = 9,
+    DictionaryError = 10,
+    UnknownError = 11,
+    ImpossibleError = 12,
+    OutOfGasError = 13,
+    OutOfGasErrorAlt = -14,
+    ActionListInvalid = 32,
+    ActionListTooLong = 33,
+    ActionInvalid = 34,
+    NotEnoughTON = 37,
+    NotEnoughExtraCurrencies = 38,
+}
+
 export class Fireworks implements Contract {
     constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
 
@@ -56,35 +103,11 @@ export class Fireworks implements Contract {
         });
     }
 
-    async sendBadMessage1(provider: ContractProvider, via: Sender, value: bigint) {
-        if (this.init === undefined) {
-            throw Error('wrong init state');
-        }
-
+    async sendBadMessage(provider: ContractProvider, via: Sender, value: bigint, msg: Cell) {
         await provider.internal(via, {
             value,
             sendMode: SendMode.PAY_GAS_SEPARATELY,
-            body: beginCell()
-                .storeUint(OPCODES.FAKED_LAUNCH, 32)
-                .storeRef(this.init.code)
-                .storeRef(beginCell().endCell())
-                .endCell(),
-        });
-    }
-
-    async sendBadMessage2(provider: ContractProvider, via: Sender, value: bigint) {
-        if (this.init === undefined) {
-            throw Error('wrong init state');
-        }
-
-        await provider.internal(via, {
-            value,
-            sendMode: SendMode.PAY_GAS_SEPARATELY,
-            body: beginCell()
-                .storeUint(OPCODES.FAKED_LAUNCH, 32)
-                .storeRef(this.init.code)
-                .storeRef(beginCell().storeUint(1, 256).storeUint(2, 256).storeUint(3, 256).storeUint(4, 255).endCell()) //1023 bits cell
-                .endCell(),
+            body: msg,
         });
     }
 
